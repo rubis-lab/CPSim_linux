069a9d7102e2189dafbc388dea3cfc28d6a00889
kswe <kswe.rubis@gmail.com>
2017-05-22 14:49:12 +0900

    Revert "Simulation engine is revised for supporting tasks whose execution times are varying."
    This reverts commit 4895bb504b9568a9e222c000175a9faac97e5979.

--- Simulator/engine/real-time_simulator_basic.cpp
+++ Simulator/engine/real-time_simulator_basic.cpp
@@ -4,8 +4,9 @@
 #include "data_list.hh"
 #include <sys/time.h>
 #include <unistd.h>
-#include <sched.h>
 
+#define TIME_WINDOW	1000000		// us
+#define OVER_TIME 1000000		// us
 #define HYPER 1;		// number of hyperperiod
 
 #define LOCATION "~/"	// path for log
@@ -20,7 +21,6 @@ int num_resources = 0;
 int num_tasks = 0;
 vector<Resource*> resources;
 vector<Task_info*> whole_tasks;
-vector<Task*> whole_task_functions;
 DAG *dag;
 Execution *execution;
 
@@ -39,11 +39,6 @@ float memory_buffer[10];
 pthread_mutex_t section_for_sending = PTHREAD_MUTEX_INITIALIZER;
 list<CAN_Msg *> waiting_data;
 
-// for execution time update
-vector<unsigned long long> task_start_times;
-vector<unsigned long long> task_finish_times;
-list<Task_info*> running_tasks;
-
 // for infinite looping
 int loop_condition = 1;
 
@@ -51,13 +46,16 @@ int loop_condition = 1;
 HANDLE hCAN1;
 
 void initialize();
-void init_CAN();
+void init_CAN(int num_channel);
 unsigned long long getcurrenttime();
 void wait(unsigned long long to);
-void* task_thread(void *arg);
 void* receive_CAN_thread(void *arg);
-void try_send_data_via_can();
+void try_send_data_via_can(FILE *fp);
+void try_write_for_schedule_plot(FILE *fp);
+void try_write_for_line_plot();
+void try_write_for_response_plot();
 void program_complete();
+void add_time_plot(Task *task, list<Time_plot *> *plot);
 int calculate_hyper_period(unsigned int start_index, unsigned int size);
 
 // main function
@@ -68,32 +66,6 @@ int main(int argc, char* argv[])
 
 	// set CAN connection for one channel
 	init_CAN(1);
-
-	// initialize thread atrributes
-    struct sched_param schedParam;
-    pthread_t taskThread;
-    pthread_attr_t attr;
-	cpu_set_t cpu;
-
-    // initialize the thread attributes
-    if (pthread_attr_init(&attr)) {
-            ERR("Failed to initialize thread attrs\n");
-            cleanup(EXIT_FAILURE);
-    }
-	// set affinity
-	CPU_ZERO(&cpu);
-	CPU_SET(3, &cpu);
-	pthread_attr_setaffinity_np(&attr, sizeof(cpu_set_t), &cpu);
-    // force the thread to use custom scheduling attributes
-    if (pthread_attr_setinheritsched(&attr, PTHREAD_EXPLICIT_SCHED)) {
-            ERR("Failed to set schedule inheritance attribute\n");
-            cleanup(EXIT_FAILURE);
-    }
-    // set the thread to be fifo real time scheduled */
-    if (pthread_attr_setschedpolicy(&attr, SCHED_FIFO)) {
-            ERR("Failed to set FIFO scheduling policy\n");
-            cleanup(EXIT_FAILURE);
-    }
 
 	// create recv thread
 	pthread_t hThread;
@@ -120,82 +92,34 @@ int main(int argc, char* argv[])
 
 		// pick the first node
 		// (1) pick a job in the simulation ready queue
-		execution->cur_time = getcurrenttime();
-		Node *cur_node = execution->get_the_first_node();
+		Node *cur_node = get_the_first_node();
 
-		// execute the job if its deadline is enough early
-		list<Node*>::iterator pos;
-		if(cur_node != NULL)
+		if(cur_node == NULL)	// no job. then jump
 		{
-			int runnable = 1;
-			int min_deadline = cur_node->effective_deadline;
-			for(pos = running_tasks.begin(); pos != running_tasks.end(); pos++)
-			{
-				if((*pos)->effective_deadline < cur_node->effective_deadline)
-				{
-					runnable = 0;
-					break;
-				}
-			}
-
-			if(runnable)
-			{
-				int running_task_id = cur_node->task->id;
-				task_finish_times[running_task_id] = 0;
-				running_tasks.push_back(cur_node->task);
-
-				// set the thread priority */
-				int relative_priority = -10 + running_tasks.size();
-				schedParam.sched_priority = sched_get_priority_max(SCHED_FIFO)-relative_priority;
-				if (pthread_attr_setschedparam(&attr, &schedParam))
-				{
-						ERR("Failed to set scheduler parameters\n");
-						cleanup(EXIT_FAILURE);
-				}
-
-				// create task thread
-				if (pthread_create(&taskThread, &attr, task_thread, (void*)&running_task_num))
-				{
-						ERR("Failed to create task thread\n");
-						cleanup(EXIT_FAILURE);
-				}
-			}
+			int min_release = get_nearest_start_time();			// get the nearest start time
+			cur_time = min_release;			// set cur_time to min_release
+			continue;
 		}
 
-		// check running task is complete
-		cur_node = NULL;
-		for(pos = running_tasks.begin(); pos != running_tasks.end(); pos++)
-		{
-			if(task_finish_times[(*pos)->task->id] > 0)		// complete
-			{
-				cur_node = (*pos);
-				
-				// get actual execution times
-				int task_id = (*pos)->task->id;
-				int execution_time = task_finish_times[task_id] - task_start_times[task_id];
-				for(int i = 0; i < whole_tasks.size(); i++)
-				{
-					if(task_start_times[i] < task_finish_times[i] && task_start_times[i] > task_start_times[task_id] && task_finish_times[i] < task_finish_times[task_id])	// this job is preempted
-						execution_time -= (task_finish_times[i] - task_start_times[i]);
-				}
+		// first start of this job
+		if(cur_node->remaining_time_PC == cur_node->actual_execution_time_PC)		// start time of this node
+			cur_node->start_time_PC = cur_time;
 
-				// set start, execution, finish time
-				cur_node->start_time_PC = task_start_times[task_id];
-				cur_node->actual_execution_time_PC = execution_time;
-				cur_node->finish_time_PC = task_finish_times[task_id];
-				running_tasks.remove(cur_node);
+		// get start time of a job who can delay cur_node
+		int next_time = get_nearest_start_time();
 
-				break;
-			}
-		}
-
-		if(cur_node != NULL)		// this node can complete
+		if(cur_node->remaining_time_PC <= (next_time-cur_time))		// this node can complete
 		{
 			cur_time += cur_node->remaining_time_PC;	// cur_time increases
 			cur_node->remaining_time_PC = 0;
 			cur_node->actual_execution_time_ECU = cur_node->actual_execution_time_PC*100/cur_node->task->modified_rate;
 			cur_node->is_executed = 1;
+			cur_node->finish_time_PC = cur_time;		// finish time update
 			next_release_of_executed_job = cur_node->release_time + dag->hyper_period;
+
+			// simulatability check
+			if(cur_node->is_virtual == 1 && cur_node->finish_time_PC > cur_node->min_finish_time_ECU)
+				return 0;	// not simulatable
 
 			// pop this job on the ready queue
 			ready_queue.remove(cur_node);
@@ -205,26 +129,28 @@ int main(int argc, char* argv[])
 
 			// gather who can be in ready queue after cur_node is executed
 			list<Node*> ready_candidates;
+			list<Node*>::iterator pos;
 			for(pos = cur_node->successors.begin(); pos != cur_node->successors.end(); pos++)
 				ready_candidates.push_back(*pos);
 			for(pos = cur_node->non_deterministic_successors.begin(); pos != cur_node->non_deterministic_successors.end(); pos++)
 				ready_candidates.push_back(*pos);
+			
 
-			// (2) add a now job into OJPG
+			// kswe. (2) add a now job into OJPG
 			if(next_release_of_executed_job >= end_time)
 				dag->pop_and_push_node(cur_node, 0);
 			else
 				dag->pop_and_push_node(cur_node);
 			
-			// (3) update start, finish times
-			execution->update_start_finish_time(cur_node);
+			// kswe. (3) update start, finish times
+			update_start_finish_time(cur_node);
 
-			// (4) adjust non-determinism
+			// kswe. (4) adjust non-determinism
 			for(pos = dag->link_updatable.begin(); pos != dag->link_updatable.end(); pos++)
-				execution->adjust_non_deterministic(*pos);
+				adjust_non_deterministic(*pos);
 
-			// (5) update effective deadlines
-			execution->update_deadlines_optimized();
+			// kswe. (5) update effective deadlines
+			update_deadlines_optimized();
 
 			// update ready queue
 			for(pos = ready_candidates.begin(); pos != ready_candidates.end(); pos++)
@@ -233,12 +159,71 @@ int main(int argc, char* argv[])
 					ready_queue.push_back(*pos);
 			}
 		}
+		else
+		{
+			cur_node->remaining_time_PC -= (next_time-cur_time);	// remaining time decreases
+			cur_time += (next_time-cur_time);					// cur_time increases
+		}
+	}
+
+	// main loop
+	while(loop_condition == 1)
+	{
 		// try sending data
-		try_send_data_via_can();
+		try_send_data_via_can(fp_chart);
+
+		int current_time_temp = current_time;
+
+		// increase simulation time
+		current_time += TIME_WINDOW;
+
+		while(loop_condition == 1)
+		{
+			// try sending data
+			try_send_data_via_can(fp_chart);
+
+			// get next executable job
+			next = execution->get_next_job(current_time_temp, &whole_tasks);
+			if(next == NULL)
+				break;
+
+			if(next->task_link->get_is_read() == 1)		// wait for time synchronization
+			{
+				do
+				{
+					try_send_data_via_can(fp_chart);
+				} while(next->get_effective_release_time() > getcurrenttime());
+			}
+
+			// calculate expected start and end time
+			actual_start_time = current_time_temp;
+			expected_end_time = actual_start_time + next->task_link->get_modified_wcet();
+
+			// jump simulation time into this job's release time
+			if(next->task_link->get_is_read() == 1 && current_time_temp < next->get_effective_release_time())
+				current_time_temp = next->get_effective_release_time() + next->task_link->get_modified_wcet();
+			else
+				current_time_temp += next->task_link->get_modified_wcet();
+
+			// execute a task (job)
+			next->read();
+			next->procedure();
+			next->write();
+
+			delete next;
+
+			// wait during modified wcet
+			do
+			{
+				try_send_data_via_can(fp_chart);
+			} while(expected_end_time > getcurrenttime());
+		}
 	}
 
 	// uninitialize phase
 	CAN_Close(hCAN1);
+	CAN_Close(hCAN2);
+	getchar();
 
 	return 0;
 }
@@ -252,12 +237,6 @@ void initialize()
 
 	num_resources = resources.size();
 	num_tasks = whole_tasks.size();
-
-	for(unsigned int i = 0; i < num_tasks; i++)
-	{
-		task_start_times.push_back(0);
-		task_finish_times.push_back(0);
-	}
 
 	int hyper_period = calculate_hyper_period(0, whole_tasks.size());
 
@@ -298,8 +277,11 @@ void initialize()
  * <argument>
  * num_channel: number of CAN channels
  */
-void init_CAN()
+void init_CAN(int num_channel)
 {
+	// can connect
+	int i = 0;
+
 	hCAN1 = LINUX_CAN_Open(PCANUSB1, O_RDWR);
 	if(!hCAN1)
 	{
@@ -316,6 +298,24 @@ void init_CAN()
 		}	
 	}
 	CAN_Status(hCAN1);
+
+	if(num_channel <= 1) return;
+
+	hCAN2 = LINUX_CAN_Open(PCANUSB2, O_RDWR);
+	if(!hCAN2)
+	{
+		cerr << "No device, USBBUS2\n";
+		exit(1);
+	}
+	if(wBTR0BTR1)
+	{
+		errno = CAN_Init(hCAN2, wBTR0BTR1, nExtended);
+		if(errno)
+		{
+			CAN_Close(hCAN2);
+			return;
+		}	
+	}
 }
 
 // This function gets the current time from the start of simulation as micro sec.
@@ -337,30 +337,6 @@ void wait(unsigned long long to)
 	}
 }
 
-// This is a thread for executing a task
-void* task_thread(void *arg)
-{
-	// set affinity
-	unsigned long mask = 3;
-
-	int task_id = *(int*)arg;
-
-	// read data
-	whole_task_functions[task_id]->read();
-
-	// check the start time
-	task_start_times[task_id] = getcurrenttime();
-
-	// execute task
-	whole_task_functions[task_id]->procedure();
-
-	// check the finish time
-	task_finish_times[task_id] = getcurrenttime();
-
-	// write data
-	whole_task_functions[task_id]->write();
-}
-
 // This is a thread for receiving CAN message.
 void* receive_CAN_thread(void *arg)
 {
@@ -373,12 +349,13 @@ void* receive_CAN_thread(void *arg)
 		// Check the receive queue for new messages
 		//
 		errno = CAN_Read(hCAN1, &msg);
-		if(errno == PCAN_ERROR_OK)
+		if(!errno)
 		{
 			unread_count = 0;
 			switch(msg.ID)
 			{
 #include "can_read.hh"
+#include "dummy.hh"
 
 				default:
 //					printf("not defined msg\n");
@@ -407,7 +384,7 @@ void* receive_CAN_thread(void *arg)
 /* This function tries to send CAN messages via CAN bus.
  * If there is no message to send at this time, do nothing.
  */
-void try_send_data_via_can()
+void try_send_data_via_can(FILE *fp)
 {
 	CAN_Msg *send_data;
 
@@ -422,8 +399,23 @@ void try_send_data_via_can()
 		waiting_data.pop_front();
 		pthread_mutex_unlock(&section_for_sending);
 
+		// print for scheduling plot
+		char *task_name = send_data->get_task_name();
+		if(send_data->get_time()/1000 > recent_time)
+		{
+			recent_time = send_data->get_time()/1000;
+		}
+		int is_start = 1;
+		fprintf(fp, "%d, msg: %s, %d\n", recent_time, task_name, is_start);
+		recent_time++;
+		is_start = 0;
+		fprintf(fp, "%d, msg: %s, %d\n", recent_time, task_name, is_start);
+
 		// The message is sent using the PCAN-USB
-		errno = CAN_Write(hCAN1, &(send_data->msg));
+		if (send_data->get_channel() == 1) 
+			errno = CAN_Write(hCAN1, &(send_data->msg));
+		else 
+			errno = CAN_Write(hCAN2, &(send_data->msg));
 		if(errno)
 		{
 			// An error occurred, get a text describing the error and show it
